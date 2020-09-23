07840c8df1efa30ad6d34057629cdac5fd000100
Gabriel Thomé <gabrielallan.thome@gmail.com>
2020-09-23 13:00:21 -0300

    add date time logic to nvgrupo command

--- src/commands/nvgrupo.ts
+++ src/commands/nvgrupo.ts
@@ -1,22 +1,32 @@
 import { Message } from "discord.js";
+import { getHours, getMinutes } from '../dateTime';
 import config from '../config';
 
 module.exports = {
   name: 'nvgrupo',
   minArgs: 2,
   guildOnly: true,
-  usage: '<horário> <nome do evento>',
+  usage: '<horário>h \"<nome do evento>\"\nex: 20h among us',
   execute(msg: Message, args: String[]) {
-    const time = args.shift();
+    const timeRegex = /^\d{1,2}(h|:)\d{0,2}$/;
+    const time = args.filter(x => timeRegex.test(x.toString()))[0];
+
+    const dateTime = new Date();
+    dateTime.setHours(parseInt(getHours(time.toString())));
+    dateTime.setMinutes(parseInt(getMinutes(time.toString())) || 0);
+
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes().toString().padStart(2, '0')
+
     const partyName = args.join(' ');
     const member = msg.member?.toString();
-    
+
     const replyEmbed = {
       title: partyName,
       fields: [,
         {
           name: 'Horário',
-          value: time,
+          value: hours + ':' + minutes,
         },
         {
           name: 'Participantes',
